da5729e2d9214eaf9b15c05857f0569ce66cc77d
sunzidanasrin <sunzidanasrin17@gmail.com>
2017-10-11 23:58:06 +0600

    practice java with shamim vai

--- CoreJavaPractice/src/megha/helloWorld/HelloWorld.java
+++ CoreJavaPractice/src/megha/helloWorld/HelloWorld.java
@@ -3,5 +3,6 @@ package megha.helloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Megha....");
+        System.out.println("Hello, Bangladesh....");
     }
 }
