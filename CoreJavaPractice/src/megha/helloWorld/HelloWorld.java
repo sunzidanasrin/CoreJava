f43efe25e35d2e58fd2514d3a007305306e5347f
sunzidanasrin <sunzidanasrin17@gmail.com>
2017-10-11 23:48:17 +0600

    Practice with java

--- CoreJavaPractice/src/megha/helloWorld/HelloWorld.java
+++ CoreJavaPractice/src/megha/helloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package megha.helloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Urmi Apu....");
+        System.out.println("Hello, Megha....");
     }
 }
