e584504b46eff478476ae01f5401f603a5ad4df1
unknown <sunzidanasrin>
2017-10-12 09:06:21 +0600

    Class practice

---
+++ ClassPractice/src/cls/thread/ThreadExample.java
@@ -0,0 +1,13 @@
+package cls.thread;
+
+
+public class ThreadExample extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Running ");
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
