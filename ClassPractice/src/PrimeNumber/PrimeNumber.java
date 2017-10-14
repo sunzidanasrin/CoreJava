3b2d318143f308c533a11dcc2c939583cb6a859a
unknown <sunzidanasrin>
2017-10-14 12:52:44 +0600

    passtest

---
+++ ClassPractice/src/PrimeNumber/PrimeNumber.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PrimeNumber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author J2EE-33
+ */
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        
+        int i, m=0, flag = 0;
+        m = n/2;
+                if(n == 0 || n == 1){
+                    System.out.println("Number is not prime");
+                    flag = 1;
+                    
+                }else{
+                    for (i = 2; i <= m; i++) {
+                        if (n % i == 0) {
+                            System.out.println("Number is not prime");
+                            flag = 1;
+                            break;
+                        }
+                        
+                    }
+                    
+                }
+                if (flag == 0){
+                    System.out.println("Number is prime");
+                }
+                
+                
+    }
+}
